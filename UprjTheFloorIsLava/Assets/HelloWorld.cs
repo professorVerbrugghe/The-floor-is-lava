dd932026c71381f9bb0c1b9edbec7c48d05a051d
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-10-28 14:08:32 +0200

    Tutorial: Code in the default script
    In this tutorial, you’ll:
    Identify the default script components of a new script
    Edit a script component in your IDE (Integrated Development Environment).
    Display a message from a script in the Unity Editor’s Console window.

--- UprjTheFloorIsLava/Assets/HelloWorld.cs
+++ UprjTheFloorIsLava/Assets/HelloWorld.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string strMyMessage;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(strMyMessage);
+       
     }
 }
